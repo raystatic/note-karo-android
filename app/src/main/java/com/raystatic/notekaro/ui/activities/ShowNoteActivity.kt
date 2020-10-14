e31927a3b97abfffc54fc7b9ff1521f31b9d814a
raystatic <rahul9650ray@gmail.com>
2020-10-14 20:29:59 +0530

    show note completed

--- app/src/main/java/com/raystatic/notekaro/ui/activities/ShowNoteActivity.kt
+++ app/src/main/java/com/raystatic/notekaro/ui/activities/ShowNoteActivity.kt
@@ -1,13 +1,55 @@
 package com.raystatic.notekaro.ui.activities
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.annotation.RequiresApi
 import com.raystatic.notekaro.R
+import com.raystatic.notekaro.data.local.notes.Note
+import com.raystatic.notekaro.other.PrefManager
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_show_note.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ShowNoteActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var prefManager: PrefManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_note)
+
+        val note = intent.getParcelableExtra<Note>("note_data")
+
+        note?.let { n->
+            tvNoteTitle.text = n.title
+            tvNoteText.text=  n.text
+        }
+
+        tvNoteTitle.apply {
+            ellipsize = TextUtils.TruncateAt.MARQUEE
+            setSingleLine()
+            marqueeRepeatLimit = 10
+            isFocusable = true
+            setHorizontallyScrolling(true)
+            isFocusableInTouchMode = true
+            requestFocus()
+        }
+
+        imgBack.setOnClickListener {
+            finish()
+        }
+
+        imgEdit.setOnClickListener {
+            val intent = Intent(this, CreateNoteActivity::class.java)
+            intent.putExtra("note_data",note)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
