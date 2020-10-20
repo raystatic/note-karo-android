788c9bf4bf23df3f3120a7da064b91f02a0b9af3
raystatic <rahul9650ray@gmail.com>
2020-10-20 13:54:19 +0530

    delete dialog ready

--- app/src/main/java/com/raystatic/notekaro/ui/activities/ShowNoteActivity.kt
+++ app/src/main/java/com/raystatic/notekaro/ui/activities/ShowNoteActivity.kt
@@ -1,16 +1,25 @@
 package com.raystatic.notekaro.ui.activities
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.WindowManager
 import androidx.annotation.RequiresApi
 import com.raystatic.notekaro.R
 import com.raystatic.notekaro.data.local.notes.Note
 import com.raystatic.notekaro.other.PrefManager
+import com.raystatic.notekaro.other.Utility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_create_note.*
 import kotlinx.android.synthetic.main.activity_show_note.*
+import kotlinx.android.synthetic.main.activity_show_note.imgBack
+import kotlinx.android.synthetic.main.activity_show_note.tvNoteTitle
+import kotlinx.android.synthetic.main.delete_confirmation_layout.view.*
+import kotlinx.android.synthetic.main.title_dialog.view.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -51,5 +60,33 @@ class ShowNoteActivity : AppCompatActivity() {
             finish()
         }
 
+        imgDelete.setOnClickListener {
+            showDeleteDialog()
+        }
+
     }
+
+    private fun showDeleteDialog() {
+        val dialog = Dialog(this)
+        val view = LayoutInflater.from(this).inflate(R.layout.delete_confirmation_layout, null)
+
+        dialog.apply {
+            setContentView(view)
+            setCancelable(true)
+            view.btnCancel.setOnClickListener {
+                this.cancel()
+            }
+
+            view.btnDelete.setOnClickListener {
+                
+            }
+        }
+
+        dialog.show()
+
+        val window = dialog.window
+        window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+
+    }
+
 }
